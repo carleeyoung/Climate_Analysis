1ed23b53f09c28981db3652df16ea9ccfa37cb80
carleeyoung <61207381+carleeyoung@users.noreply.github.com>
2020-06-10 17:57:22 -0500

    Updated README and presentation

--- config.py
+++
@@ -1,7 +0,0 @@
-NOAA_api_key = "UqedtDOOiFbNISchZsumYBYGkuHpTfPW"
-
-census_key = "6fe00bdaa5188deca90fb6d330130425d27c1056"
-
-api_key = "9137339baa60e3b516672939c56b42f7"
-
-gmaps_api_key = "AIzaSyBM6mGSqcnltvZs1PPZ0YSIWf0FbFGjGKc"
